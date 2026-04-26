7c1a34c50f9615ca8224eefa0987fcd1e668bd03
Islomov-Diyor <diyorbekshaxrisabz@gmail.com>
2026-04-26 23:02:33 +0500

    refactor: extract levels into src/levels/

---
+++ src/levels/index.js
@@ -0,0 +1,8 @@
+import { grid as grid1, config as config1 } from './level1.js';
+import { grid as grid2, config as config2 } from './level2.js';
+import { grid as grid3, config as config3 } from './level3.js';
+import { grid as grid4, config as config4 } from './level4.js';
+import { grid as grid5, config as config5 } from './level5.js';
+
+export const LEVEL_GRIDS = [grid1, grid2, grid3, grid4, grid5];
+export const LEVELS      = [config1, config2, config3, config4, config5];
